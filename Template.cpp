2e17e872735756ac700fb2c6b8f2c0927dbe02a0
Danila Usachev <duesna897@gmail.com>
2022-06-07 22:20:25 +0300

    New structure

---
+++ Template.cpp
@@ -0,0 +1,92 @@
+#include <assert.h>
+#include <math.h>
+#include <time.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "acm/debug.hpp"
+#else
+#define DEBUG(...) ;
+#define ADEBUG(a, n) ;
+#endif
+
+#define ALL(a) (a).begin(), (a).end()
+#define TIME (static_cast<long double>(clock()) / CLOCKS_PER_SEC)
+
+#define int ll
+using ll = long long;
+using ull = unsigned long long;
+using dbl = double;
+using ld = long double;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+template <typename T1, typename T2>
+bool chkmin(T1& x, T2 y) {
+    if (y < x) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+template <typename T1, typename T2>
+bool chkmax(T1& x, T2 y) {
+    if (y > x) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+
+#ifndef LOCAL
+template <typename T1, typename T2>
+ostream& operator<<(ostream& stream, const pair<T1, T2>& p) {
+    return stream << p.first << ' ' << p.second;
+}
+
+template <typename T>
+ostream& operator<<(ostream& stream, const vector<T>& v) {
+    for (auto& e : v) stream << e << ' ';
+    return stream;
+}
+#endif
+
+void runSolution(void);
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    runSolution();
+#ifdef LOCAL
+    // cerr << "Solution ran in " << TIME << endl;
+#endif
+    return 0;
+}
+/***************************************************************************/
+
+void runSolution() {}
