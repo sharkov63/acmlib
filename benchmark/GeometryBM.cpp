bc35b9ba631a42908414a6192a2158547addd349
Danila Usachev <duesna897@gmail.com>
2022-07-23 17:11:57 +0300

    Added proper Geometry::Real benchmarks

--- benchmark/GeometryBM.cpp
+++ benchmark/GeometryBM.cpp
@@ -3,8 +3,6 @@
 #include "Geometry.cpp"
 #include "benchmark/benchmark.h"
 
-const int N = 10000;
-
 template <typename T>
 static void BM_GeometryRealAddition(benchmark::State& state) {
     for (auto _ : state) {
@@ -19,10 +17,62 @@ static void BM_GeometryRealAddition(benchmark::State& state) {
         benchmark::DoNotOptimize(8 - x);
         benchmark::DoNotOptimize(-x + 3);
         benchmark::DoNotOptimize(x += (x + 1e7));
+        T y = 2;
+        benchmark::DoNotOptimize(++y);
+        benchmark::DoNotOptimize(x++);
+        benchmark::DoNotOptimize(--x);
+        benchmark::DoNotOptimize(y--);
+        benchmark::DoNotOptimize(+y);
+        benchmark::DoNotOptimize(-x);
     }
 }
 
-BENCHMARK(BM_GeometryRealAddition<float>);
-BENCHMARK(BM_GeometryRealAddition<double>);
+template <typename T>
+static void BM_GeometryRealMultiplication(benchmark::State& state) {
+    for (auto _ : state) {
+        T x = 3, y = 4;
+        benchmark::DoNotOptimize(x * y);
+        benchmark::DoNotOptimize(x * (-1.4));
+        benchmark::DoNotOptimize(3 * y);
+        benchmark::DoNotOptimize(y *= 10);
+        benchmark::DoNotOptimize(y *= 1e-8);
+        benchmark::DoNotOptimize(x *= (y *= y));
+    }
+}
+
+template <typename T>
+static void BM_GeometryRealDivision(benchmark::State& state) {
+    for (auto _ : state) {
+        T x = 123;
+        T y = -78.848924;
+        benchmark::DoNotOptimize(x / y);
+        benchmark::DoNotOptimize(x / 89.333);
+        benchmark::DoNotOptimize(54 / y);
+        benchmark::DoNotOptimize(x /= (y / 3));
+        benchmark::DoNotOptimize(x /= x);
+        benchmark::DoNotOptimize(x /= (y /= 3));
+    }
+}
+
+template <typename T>
+static void BM_GeometryRealComparison(benchmark::State& state) {
+    for (auto _ : state) {
+        T x = -9.8;
+        T y = 13;
+        benchmark::DoNotOptimize(x < y);
+        benchmark::DoNotOptimize(y > -14);
+        benchmark::DoNotOptimize(x == -9.9);
+        benchmark::DoNotOptimize(x != 2 * y);
+        benchmark::DoNotOptimize(7 <= x);
+        benchmark::DoNotOptimize(y >= 13);
+    }
+}
+
 BENCHMARK(BM_GeometryRealAddition<long double>);
 BENCHMARK(BM_GeometryRealAddition<Real>);
+BENCHMARK(BM_GeometryRealMultiplication<long double>);
+BENCHMARK(BM_GeometryRealMultiplication<Real>);
+BENCHMARK(BM_GeometryRealDivision<long double>);
+BENCHMARK(BM_GeometryRealDivision<Real>);
+BENCHMARK(BM_GeometryRealComparison<long double>);
+BENCHMARK(BM_GeometryRealComparison<Real>);
